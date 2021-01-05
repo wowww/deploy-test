7c45f307c556e2e6e59d4f8fce81536963ac6952
Kwon Eun Jee <passby_time@naver.com>
2021-01-06 04:12:08 +0900

    docs. 파일 오탈자 client로 수정

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from "socket.io-client";
+
+import './Chat.scss';
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:4000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    // socket = io.connect(ENDPOINT);
+
+    setRoom(room);
+    setName(name);
+    
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+  
+  return (
+    <h1>Chat</h1>
+  )
+}
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -10,14 +10,32 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-  console.log('We have a new connetion!');
-  
-  socket.on('disconnect', () => {
-    console.log('User had left!');
-  })
-})
-
+// app.use(cors());
 app.use(router);
+
+// io.on('connect', (socket) => {
+//   console.log('We have a new connetion!');
+
+//   socket.on('join', ({ name, room }) => {
+//     console.log(name, room);
+//   })
+  
+//   socket.on('disconnect', () => {
+//     console.log('User had left!');
+//   })
+// })
+io.on('connection', function(socket) {
+  // Listen for test and disconnect events
+  socket.on('test', (data) => {
+    console.log('received: "' + data + '" from client' + socket.id);
+    socket.emit('test', "Ok, i got it, " + socket.id);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected from ', socket.id);
+  });
+});;
+
+// app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
