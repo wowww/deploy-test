9974cb0629ae23ff1190442da8c250644674a876
Kwon Eun Jee <passby_time@naver.com>
2021-01-11 15:20:54 +0900

    docs. Messages, message 랜딩 실패로 인한 Error 찾는 중

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -57,8 +57,8 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
           <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+          {/* <Messages messages={messages} name={name} /> */}
+          {/* <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
       </div>
       <TextContainer users={users}/>
     </div>
